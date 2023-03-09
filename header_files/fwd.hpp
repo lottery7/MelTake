93ff239f341bd38644ed6f052ad9734df4d7f8a8
Ksenia Khokhlova <52651781+Ksenia-Kho@users.noreply.github.com>
2023-03-09 18:23:58 +0300

    Create fwd.hpp

---
+++ header_files/fwd.hpp
@@ -0,0 +1,10 @@
+#ifndef FWD_HPP
+#define FWD_HPP
+
+#include <playlist_structure.hpp>
+#include <track_structure.hpp>
+#include <settings_structure.hpp>
+
+namespace audio_app{
+}
+#endif //FWD_HPP
