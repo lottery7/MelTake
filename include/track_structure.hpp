fd57f38a69c1c9e8030bdadc67b56d687081d8c1
Ekaterina Badaeva <112130616+B-E-D-A@users.noreply.github.com>
2023-03-21 00:26:54 +0300

    Update track_structure.hpp

--- include/track_structure.hpp
+++ include/track_structure.hpp
@@ -2,13 +2,21 @@
 #define TRACK_STRUCTURE_HPP
 
 #include <iostream>
+#include <utility>
+#include "fwd.hpp"
 
 namespace audio_app {
 
 struct track {
-    std::string track_name;
+    std::string m_track_name;
+    std::string m_track_path;
 
-    track() : track_name() {
+    track() : m_track_name() {
+    }
+
+    explicit track(std::string track_name, std::string track_path)
+        : m_track_name(std::move(track_name)),
+          m_track_path(std::move(track_path)) {
     }
 
     track(track &&another) noexcept = default;
@@ -20,6 +28,8 @@ struct track {
     track &operator=(const track &) = delete;
 
     ~track() = default;
+
+    //    friend bool operator==(const track &lhs, const track &rhs);
 
     void play_track(const track &track_);
 
