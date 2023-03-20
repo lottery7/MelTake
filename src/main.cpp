6469bc52a2de27101c6960238db077ad4b5a962d
Ekaterina Badaeva <112130616+B-E-D-A@users.noreply.github.com>
2023-03-21 00:27:15 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
-#include <fwd.hpp>
+#include "include/fwd.hpp"
 
 int main() {
+    // upload user's track_data from previous usage
+    audio_app::music_base user_music_base;
+    user_music_base.beginning_setup();
+    user_music_base.upload_info_from_trackdata_file();
+    // how the app structure looks
+    user_music_base.app_view();
 }
