0125bd8a2b2a78632708c85a61c9396db9cc897f
Ekaterina Badaeva <112130616+B-E-D-A@users.noreply.github.com>
2023-03-21 00:28:20 +0300

    Update track_functions.cpp

--- src/track_functions.cpp
+++ src/track_functions.cpp
@@ -1,13 +1,14 @@
-#include <fwd.hpp>
+#include <include/fwd.hpp>
+#include "include/track_structure.hpp"
 
 using namespace audio_app;
 
 [[maybe_unused]] void track::edit_track(track &track_) {
-    std::cout << "enter a new name for the track " << track_.track_name
+    std::cout << "enter a new name for the track " << track_.m_track_name
               << ":\n";
     std::string new_track_name;
     std::cin >> new_track_name;
-    track_.track_name = new_track_name;
+    track_.m_track_name = new_track_name;
 }
 
 [[maybe_unused]] void track::play_track(const track &track_) {
@@ -17,3 +18,8 @@ using namespace audio_app;
 [[maybe_unused]] void track::stop_track(const track &track_) {
     // pause track
 }
+
+//
+// bool operator==(const track & lhs, const track &rhs) {
+//    return lhs.m_track_name==rhs.m_track_name;
+//}
