93522a9625da2dda4923c3a633e126d60e920de6
Ekaterina Badaeva <112130616+B-E-D-A@users.noreply.github.com>
2023-03-21 00:28:00 +0300

    Update settings_functions.cpp

--- src/settings_functions.cpp
+++ src/settings_functions.cpp
@@ -1,3 +1,17 @@
-#include <fwd.hpp>
+#include <include/fwd.hpp>
+#include "include/settings_structure.hpp"
 
 using namespace audio_app;
+
+void settings::set_theme(int type) {
+    theme_type = type;
+}
+
+void settings::set_visualisation(int type) {
+    visualisation_type = type;
+}
+
+void settings::back_to_default() {
+    theme_type = 0;
+    visualisation_type = 0;
+}
