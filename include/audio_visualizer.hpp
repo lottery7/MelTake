67cc624e44cba5b86afe76a2687d8fcf0d429e34
Eugene Akimov <akimoveugeny.04@mail.ru>
2023-04-15 10:32:42 +0300

    Optimization and improvement of animation.
    1) Optimization. Animation looks more smoothly.
    2) Improvement of frequency decomposition.
    3) Some audio files can become buggy at this update

--- include/audio_visualizer.hpp
+++ include/audio_visualizer.hpp
@@ -1,13 +1,16 @@
 #ifndef AUDIO_VISUALIZER_H
 #define AUDIO_VISUALIZER_H
 
-#include <QWidget>
+
 #include <QAudioProbe>
 #include <QMediaPlayer>
 #include <QTimer>
 #include <QQueue>
-#include <QList>
 #include <QVector>
+#include <QWidget>
+
+// #include <QOpenGLWidget>
+// #include <QOpenGLFunctions>
 
 #include <fftw3.h>
 
@@ -19,70 +22,38 @@ class audio_visualizer : public QWidget {
 public:
     explicit audio_visualizer(QWidget *parent = nullptr);
 
-    ~audio_visualizer() {
-        freeFFT();
-    }
-
     void set_media_player(QMediaPlayer *player) {
         m_internal_player = player;
         m_probe.setSource(m_internal_player);
-        // connect(m_internal_player, SIGNAL(mediaChanged()), this, SLOT(restore_resources()));
     }
-
-    void set_audio_format(QAudioFormat format);
     
 protected:
+    // void initializeGL() override;
+    // void resizeGL(int, int) override;
+    // void paintGL() override;
+
     void paintEvent(QPaintEvent *) override;
 
 private:
-    QVector<double> compute_magnitudes(const QVector<double> &audio_data);
-    QVector<double> normalize_data(const void *raw_data, int start, int end);
-    int get_samples_per_chunk();
-
-    // bool should_skip_chunk() {
-    //     return m_audio_pos > m_chunk_cur_pos + m_chunk_duration_ms;
-    // }
-    
-    void freeFFT() {
-        fftw_destroy_plan(m_fft_plan);
-        fftw_free(m_fft_input);
-        fftw_free(m_fft_output);
+    qint64 get_audio_pos() {
+        return m_internal_player->position();
     }
 
-    void initFFT(int n) {
-        m_fft_input = fftw_alloc_real(2 * n);
-        m_fft_output = fftw_alloc_complex(n);
-        m_fft_plan = fftw_plan_dft_r2c_1d(n, m_fft_input, m_fft_output, FFTW_MEASURE | FFTW_DESTROY_INPUT);
-    }
-    
     QMediaPlayer *m_internal_player;
     QAudioProbe m_probe;
     QAudioFormat m_audio_format;
-    qint64 m_audio_pos;
 
-    QVector<double> m_magnitudes;
-    QQueue<QVector<double>> m_chunk_queue;
-    qint64 m_chunk_duration_ms;
-    qint64 m_chunk_pos;
-    qint64 m_chunk_processed_pos;
-    qint32 m_skip_chunks;
+    double *m_dft_input;
+    fftw_complex *m_dft_output;
+    fftw_plan m_dft_plan;
+    qint64 m_dft_input_index;
 
-    double *m_fft_input;
-    fftw_complex *m_fft_output;
-    fftw_plan m_fft_plan;
-
-    QTimer m_update_timer;
+    QQueue<double> m_samples_queue;
 
 private slots:
     void process_buffer(const QAudioBuffer &buffer);
-
-    void update_audio_pos() {
-        m_audio_pos = m_internal_player->position();
-    }
-
-    // void restore_resources();
 };
 
-}  // namespace auvi
+}  // namespace audio_app
 
 #endif  // AUDIO_VISUALIZER_H
--- src/audio_visualizer.cpp
+++ src/audio_visualizer.cpp
@@ -8,68 +8,52 @@
 #include <QTimer>
 #include <QVector2D>
 #include <QCoreApplication>
+#include <QSysInfo>
+#include <QtGlobal>
 
 #include <algorithm>
+#include <limits>
+#include <complex>
 #include <fftw3.h>
+
+
+#define DFT_SIZE 1024
+#define SAMPLE_RATE 44100
+#define SAMPLE_SIZE 16
+#define SAMPLE_TYPE qint16
+#define MAX_MAGNITUDE 10'000
 
 namespace audio_app {
 
-const qint32 FPS = 50;
-const qint32 MSPF = 1000 / FPS;
-const double FREQ_SCALE = 100.0;
+const double FPS = 50.;
+const double MSPF = 1000. / FPS;
+const double FREQ_SCALE = 10.0;
 
 /*--PUBLIC--*/
 
 audio_visualizer::audio_visualizer(QWidget *parent) 
     : QWidget(parent)
     , m_probe(this)
-    , m_chunk_duration_ms(MSPF)
-    , m_chunk_pos(0)
-    , m_chunk_processed_pos(0)
-    , m_update_timer(this)
+    , m_dft_input(fftw_alloc_real(DFT_SIZE * 2))
+    , m_dft_output(fftw_alloc_complex(DFT_SIZE))
+    , m_dft_input_index(0)
 {
     setWindowTitle("Visualizer");
 
+    m_dft_plan = fftw_plan_dft_r2c_1d(DFT_SIZE, m_dft_input, m_dft_output, FFTW_MEASURE);
+
     connect(&m_probe, SIGNAL(audioBufferProbed(QAudioBuffer)), this, SLOT(process_buffer(QAudioBuffer)));
 
-    connect(&m_update_timer, SIGNAL(timeout()), this, SLOT(update()));
-    connect(&m_update_timer, SIGNAL(timeout()), this, SLOT(update_audio_pos()));
-
-    m_update_timer.start(MSPF);
-}
-
-void audio_visualizer::set_audio_format(QAudioFormat format) {
-    Q_ASSERT(format.isValid());
-
-    m_audio_format = format;
-    int samples_count = get_samples_per_chunk();
-    
-    freeFFT();
-    initFFT(samples_count);
 }
 
 /*--PROTECTED--*/
 
 void audio_visualizer::paintEvent(QPaintEvent *) {
-    while (m_chunk_pos + m_chunk_duration_ms < m_audio_pos) {
-        if (!m_chunk_queue.empty()) {
-            m_magnitudes = m_chunk_queue.dequeue();
-            m_chunk_pos += m_chunk_duration_ms;
-        } else {
-            m_skip_chunks = 1 + (m_audio_pos - m_chunk_pos) / m_chunk_duration_ms;
-            m_chunk_pos += m_skip_chunks * m_chunk_duration_ms;
-        }
+    if (m_dft_input_index >= DFT_SIZE) {
+        fftw_execute(m_dft_plan);
     }
 
-    if (m_magnitudes.empty()) {
-        if (m_chunk_queue.empty() || m_chunk_pos >= m_audio_pos) {
-            return;
-        }
-        m_magnitudes = m_chunk_queue.dequeue();
-        m_chunk_pos += m_chunk_duration_ms;
-    }
-
-    // qDebug() << m_chunkCurrentPosition << m_audio_pos;
+    std::complex<double> *dft_data = reinterpret_cast<std::complex<double> *>(m_dft_output);
 
     QPainter painter(this);
     painter.setPen(Qt::black);
@@ -80,115 +64,63 @@ void audio_visualizer::paintEvent(QPaintEvent *) {
     int dy = (height() - h) / 2;
 
     // Calculate the number of frequency values and the spacing between them
-    int num_freq = m_magnitudes.size();
+    qint64 num_freq = DFT_SIZE / 2;
     double freq_spacing = static_cast<double>(w) / num_freq;
     // double max_value = *std::max_element(m_magnitudes.constBegin(), m_magnitudes.constEnd());
 
+    double current_magnitude;
+    double next_magnitude = std::abs(dft_data[1]);
+    
     // Iterate through each frequency value and draw a line between adjacent points
-    for (int i = 0; i < num_freq - 1; i++)
+    for (qint64 i = 1; i < num_freq - 1; i++)
     {
+        // Calculate magnitudes for 2 adjacent frequencies
+        current_magnitude = next_magnitude;
+        next_magnitude = std::abs(dft_data[i + 1]);
+
         // Calculate the x and y positions of the current and next frequency values
         int x1 = dx + i * freq_spacing;
         int x2 = dx + (i + 1) * freq_spacing;
-        // int y1 = h + dy - (int)(qPow(m_magnitudes[i] / max_value, 0.5) * h);
-        // int y2 = h + dy - (int)(qPow(m_magnitudes[i + 1] / max_value, 0.5) * h);
-        int y1 = h + dy - (int)(FREQ_SCALE * qLn(m_magnitudes[i] + 1));
-        int y2 = h + dy - (int)(FREQ_SCALE * qLn(m_magnitudes[i+1] + 1));
+
+        // int y1 = dy + h - (int)(FREQ_SCALE * qLn(current_magnitude + 1));
+        // int y2 = dy + h - (int)(FREQ_SCALE * qLn(next_magnitude + 1));
+        int y1 = dy + h - (int)(FREQ_SCALE * (1. * m_internal_player->volume() / 10) * qLn(current_magnitude / MAX_MAGNITUDE + 1));
+        int y2 = dy + h - (int)(FREQ_SCALE * (1. * m_internal_player->volume() / 10) * qLn(next_magnitude / MAX_MAGNITUDE + 1));
 
         // Draw a line between the two frequency values
         painter.drawLine(x1, y1, x2, y2);
     }
+
+    emit update();
 }
+
+// void initializeGL() {
+
+// }
+
+// void resizeGL(int, int) {
+
+// }
+
+// void paintGL() {
+    
+// }
 
 /*--PRIVATE--*/
 
-QVector<double> audio_visualizer::compute_magnitudes(const QVector<double> &data) {
-    // Data contains samples
-    int samples_count = data.size();
-
-    Q_ASSERT(samples_count == get_samples_per_chunk());
-
-    // Copy and prepare data
-    for (int i = 0; i < samples_count; ++i) {
-        // Apply Hann window function to avoid spectrum leakage
-        double windowing = 0.5 - 0.5 * cos(2 * M_PI * i / (samples_count - 1));
-        m_fft_input[2 * i] = data[i] * windowing;
-        m_fft_input[2 * i + 1] = 0;  // We have real values, so im. part equals zero
-    }
-
-    // Perform FFT
-    fftw_execute(m_fft_plan);
-
-    // One half of the output is empty, another half is symmetric
-    QVector<double> magnitudes(samples_count / 2);
-    for (int i = 0; i < samples_count / 2; ++i) {
-        double re = m_fft_output[i][0];
-        double im = m_fft_output[i][1];
-        magnitudes[i] = qSqrt(re * re + im * im);
-    }
-
-    return magnitudes;
-}
-
-QVector<double> audio_visualizer::normalize_data(const void *raw_data, int start, int end) {
-    int samples_count = end - start;
-
-    Q_ASSERT(samples_count == get_samples_per_chunk());
-
-    int sample_size = m_audio_format.sampleSize();
-    double max_value = (1 << (sample_size - 1)) - 1;
-    QVector<double> audio_data(samples_count);
-
-    int channel_count = m_audio_format.channelCount();
-
-    for (int i = start; i < end; ++i) {
-        if (sample_size == 8) {
-            audio_data[i - start] = ((qint8 *)raw_data)[i * channel_count] / max_value;
-        } else if (sample_size == 16) {
-            audio_data[i - start] = ((qint16 *)raw_data)[i * channel_count] / max_value;
-        } else {
-            qDebug() << "Audio format is not supported: sample size is" << sample_size << "which is too high";
-            return {};
-        }
-    }
-
-    return audio_data;
-}
-
-// Returns samples count per chunk IN ONE CHANNEL.
-int audio_visualizer::get_samples_per_chunk() {
-    Q_ASSERT(m_audio_format.isValid());
-
-    return m_audio_format.bytesForDuration(m_chunk_duration_ms * 1'000) * 8
-            / m_audio_format.sampleSize()
-            / m_audio_format.channelCount();
-}
-
 void audio_visualizer::process_buffer(const QAudioBuffer &buffer) {
-    if (!m_audio_format.isValid()) {
-        set_audio_format(buffer.format());
-    }
+    const SAMPLE_TYPE *raw_data = reinterpret_cast<const SAMPLE_TYPE *>(buffer.constData());
 
-    Q_ASSERT(m_audio_format.isValid());
+    Q_ASSERT(SAMPLE_RATE == buffer.format().sampleRate());
+    Q_ASSERT(SAMPLE_SIZE == buffer.format().sampleSize());
 
-    const void *raw_audio_data = buffer.constData();
-    int samples_per_chunk = get_samples_per_chunk();
-    int total_chunks = buffer.sampleCount()
-                    / m_audio_format.channelCount()
-                    / samples_per_chunk;
-
-    for (int i = 0; i < total_chunks; ++i) {
-        if (m_skip_chunks > 0) {
-            m_skip_chunks--;
-            continue;
-        }
-        QVector<double> data = normalize_data(
-            raw_audio_data,
-            i * samples_per_chunk
-            , (i + 1) * samples_per_chunk
-        );
-        m_chunk_queue.enqueue(compute_magnitudes(data));
+    for (int i = 0; i < buffer.sampleCount(); i += buffer.format().channelCount()) {
+        int input_index = (m_dft_input_index) % DFT_SIZE;
+        double windowing = 0.5 * (1.0 - qCos(2.0 * M_PI * input_index / (DFT_SIZE - 1)));
+        m_dft_input[2 * input_index] = raw_data[i] * windowing;
+        m_dft_input[2 * input_index + 1] = 0;
+        ++m_dft_input_index;
     }
 }
 
-}  // namespace auvi
+}  // namespace audio_app
